00c38e6b71099b77dbc4bbd8f0eb8ae7d8384d58
RakeshKumarParida <rakeshkumarparida424@gmail.com>
2022-05-23 19:26:51 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "./Components/Header";
+
+class App extends React.Component{
+    render() {
+        return (
+            <div>
+                <Header name = "Rakesh Kumar Parida !"/>
+            </div>
+        );
+    };
+};
+
+export default App;
